efa1de7cee6ceb3608e22b8eb2499381ae949287
torrosyan99 <torosyan.ha@gmail.com>
2026-06-02 12:41:45 +0400

    add

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
+import fs from 'fs'
+
+
+function getInputs() {
+  const files = fs.readdirSync('.')
+  const inputs = {}
+
+  files.forEach(file => {
+    if (file.endsWith('.html')) {
+      inputs[file.replace('.html', '')] = file
+    }
+  })
+
+  return inputs
+}
+
 // vite.config.js
 export default {
   build: {
+    rollupOptions: {
+      input: getInputs(),
+    },
     sourcemap: true,
     minify: false,
     terserOptions: {
       format: {
-        beautify: true
+        beautify: false
       }
-    },
+    }
   }
 }
